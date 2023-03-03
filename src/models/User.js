bc3604eaeb840773a8ab9e0dedb79b63a1091a4a
Guilherme <grsguiga@gmail.com>
2023-03-03 18:34:23 -0300

    Requisito 02

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+module.exports = (sequelize, DataTypes) => {
+    const Users = sequelize.define('User', {
+        id: DataTypes.INTEGER,
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING
+    }, {
+        timestamps: false,
+        underscored: true,
+    });
+    return Users
+}
